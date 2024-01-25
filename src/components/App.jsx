c30951ecb3d732ad49a16f92c9e8058b4cff54f9
KovalUser19 <134967853+KovalUser19@users.noreply.github.com>
2024-01-25 21:42:43 +0200

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,6 +7,7 @@ import { Route, Routes } from "react-router-dom";
 import { Layout } from "./Layout";
 import { RestrictedRoute } from "./RestrictedRoute";
 import { PrivateRoute } from "./PrivatRoute";
+import { Navigation } from "./Navigation/Navigation";
 
 const Home = lazy(() => import('../pages/HomePage'));
 const Register = lazy(() => import('../pages/RegisterPage'));
@@ -28,12 +29,11 @@ export const App = () => {
       <Routes>
       <Route path="/" element={<Layout/>}>
         <Route index element={<Home />}/>
-          <Route path="/register" element={<RestrictedRoute redirectTo="/register" component={<Register/>} />} />
+          <Route path="/register" element={<RestrictedRoute redirectTo="/contacts" component={<Register/>} />} />
           <Route path="/login" element={<RestrictedRoute redirectTo="/contacts" component={<Login />} />} />
-          <Route path="/contacts" element={<PrivateRoute redirectTo="/contacts" component={<Contacts />} />} />
-          <Route path="/logout" element={<RestrictedRoute redirectTo="/" component={<Home />} />} />
+          <Route path="/contacts" element={<PrivateRoute redirectTo="/login" component={<Contacts/>} />} />
         </Route>
-        <Route path="*" element={<h1>Error</h1>} />
+        <Route path="*" element={<Navigation/>} />
     </Routes>
   );
 
